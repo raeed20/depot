6c0c17a8a1702071379e37552d1a13836f704a37
Raed Marouf <raeed20@windowslive.com>
2016-02-13 08:13:16 -0500

    modification

--- code/main.R
+++ code/main.R
@@ -1,9 +1,10 @@
 setwd('/home/depot')
 
+#read data
+
 train = read.csv('data/train.csv',stringsAsFactors = F)
 test = read.csv('data/test.csv',stringsAsFactors = F)
 attributes = read.csv('data/attributes.csv',stringsAsFactors = F)
 product_descriptions = read.csv('data/product_descriptions.csv',stringsAsFactors = F)
 sample_submission = read.csv('data/sample_submission.csv',stringsAsFactors = F)
-
 
